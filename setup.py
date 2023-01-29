bddcd8186638e35dbd2fe7f688f407743ca4cbcd
Gourieff <85128026+Gourieff@users.noreply.github.com>
2023-01-29 23:03:50 +0700

    Update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
+"""
+Image Palette Detector
+-----------
+
+Fast image RGB palette detector by Gourieff.
+
+Links
+`````
+
+* `github <https://github.com/Gourieff/img-palette-detector'>`
+
+"""
+
 from setuptools import setup
 
-setup(name='Image Palette Detector',
+setup(name='img-pd',
       version='0.1',
       description='Fast image RGB palette detector by Gourieff',
       url='https://github.com/Gourieff/img-palette-detector',
       author='Gourieff',
       author_email='gourieff@gmail.com',
       license='GPL-3.0',
-      packages=['ipd'],
+      packages=['modules'],
+      py_modules=['ipd'],
+      install_requires=[
+        'opencv-python',
+        'colorthief'
+      ],
       zip_safe=False)
