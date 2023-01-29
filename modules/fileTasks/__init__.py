9678c56d5f5aa1af199178f4d654865c6adf5207
Gourieff <85128026+Gourieff@users.noreply.github.com>
2023-01-29 19:41:15 +0700

    Upload modules/fileTasks/__init__.py

--- modules/fileTasks/__init__.py
+++ modules/fileTasks/__init__.py
@@ -1,1 +1,26 @@
+import os,sys
+from PIL import Image, ImageDraw
 
+def is_f_exist(fname):
+    # check file exists or not
+    if os.path.exists(fname): print('File exists -- ok')
+    else: sys.exit(fname + ' does not exist')
+
+def f_remove(fname):
+    if os.path.exists(fname): os.remove(fname)
+
+def f_split(fname):
+    split_f = os.path.splitext(fname)
+    img_n = split_f[0]
+    img_e = split_f[1].lower()
+    if img_e == '.png' or img_e == '.jpg' or img_e == '.bmp': 
+        print(f'File extension: {img_e}')
+        return img_n, img_e
+    else: sys.exit(img_e + ' is wrong extension of image file')
+
+def img_resolution(fname):
+    # load the image through PIL
+    image = Image.open(fname)
+    # get img resolution
+    width, height = image.size
+    return image, width, height
